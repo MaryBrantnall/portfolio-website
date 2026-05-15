c00f29a5d5f753603b1072b32dfe65b0879fed9d
marybrantnall <marybrantnall@gmail.com>
2026-05-15 11:43:26 -0300

    getting ebird data

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,11 +1,12 @@
 import GitHubRepos from "../../components/GitHubRepos/GitHubRepos"
+import EBirdData from "../../components/EBird/EBirdData"
 
 const Projects = () => {
   return (
     <div>
       <GitHubRepos />
+      <EBirdData />
     </div>
-    
   )
 }
 
